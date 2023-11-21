698339b4eafddc61d726c4760b452855d2855b83
ぬにふちさか <nunifuchisaka@gmail.com>
2023-11-21 12:57:39 +0900

    a

---
+++ docs/cluster_script.js
@@ -0,0 +1,40 @@
+class Vector3 {
+
+}
+
+class ClusterScript {
+  
+  #deltaTime = 0;
+  state;
+
+  onUpdate() {
+    return ++this.#deltaTime;
+  }
+  getPosition() {
+    
+  }
+}
+const $ = new ClusterScript();
+
+
+//
+const hayasa = 1.0;
+const nagasa = 2.0;
+const muki = new Vector3(1,0,0);
+
+$.onUpdate(deltaTime => {
+  if (!$.state.shokika) {
+    $.state.shokika = true;
+    $.state.ichi = $.getPosition();
+    $.state.time = 0.0;
+  }
+
+  $.state.time += deltaTime;
+
+  let _ichi = $.state.ichi.clone();
+  let _muki = muki.clone();
+  
+  const nagasa_now = Math.sin($.state.time * hayasa) * nagasa;
+  _ichi.add(_muki.multiplyScalar(nagasa_now));
+  $.setPosition(_ichi);
+});
